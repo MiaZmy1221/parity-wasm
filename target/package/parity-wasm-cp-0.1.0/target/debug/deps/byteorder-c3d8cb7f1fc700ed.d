4e4d1b08d9518b1e0e372c5e314bc777f0f9193c
mengya <zhang.9407@osu.edu>
2018-10-14 10:18:26 -0400

    update

---
+++ target/package/parity-wasm-cp-0.1.0/target/debug/deps/byteorder-c3d8cb7f1fc700ed.d
@@ -0,0 +1,6 @@
+/home/mengya/parity-wasm/target/package/parity-wasm-cp-0.1.0/target/debug/deps/libbyteorder-c3d8cb7f1fc700ed.rlib: /home/mengya/.cargo/registry/src/github.com-1ecc6299db9ec823/byteorder-1.2.6/src/lib.rs /home/mengya/.cargo/registry/src/github.com-1ecc6299db9ec823/byteorder-1.2.6/src/io.rs
+
+/home/mengya/parity-wasm/target/package/parity-wasm-cp-0.1.0/target/debug/deps/byteorder-c3d8cb7f1fc700ed.d: /home/mengya/.cargo/registry/src/github.com-1ecc6299db9ec823/byteorder-1.2.6/src/lib.rs /home/mengya/.cargo/registry/src/github.com-1ecc6299db9ec823/byteorder-1.2.6/src/io.rs
+
+/home/mengya/.cargo/registry/src/github.com-1ecc6299db9ec823/byteorder-1.2.6/src/lib.rs:
+/home/mengya/.cargo/registry/src/github.com-1ecc6299db9ec823/byteorder-1.2.6/src/io.rs:
---
+++ target/package/parity-wasm-cp-0.1.0/target/debug/deps/parity_wasm_cp-f17ffb9ab9005cac.d
@@ -0,0 +1,31 @@
+/home/mengya/parity-wasm/target/package/parity-wasm-cp-0.1.0/target/debug/deps/libparity_wasm_cp-f17ffb9ab9005cac.rlib: src/lib.rs src/elements/mod.rs src/elements/primitives.rs src/elements/module.rs src/elements/section.rs src/elements/types.rs src/elements/import_entry.rs src/elements/export_entry.rs src/elements/global_entry.rs src/elements/ops.rs src/elements/func.rs src/elements/segment.rs src/elements/index_map.rs src/elements/name_section.rs src/elements/reloc_section.rs src/builder/mod.rs src/builder/invoke.rs src/builder/module.rs src/builder/code.rs src/builder/misc.rs src/builder/import.rs src/builder/memory.rs src/builder/table.rs src/builder/export.rs src/builder/global.rs src/builder/data.rs src/io.rs
+
+/home/mengya/parity-wasm/target/package/parity-wasm-cp-0.1.0/target/debug/deps/parity_wasm_cp-f17ffb9ab9005cac.d: src/lib.rs src/elements/mod.rs src/elements/primitives.rs src/elements/module.rs src/elements/section.rs src/elements/types.rs src/elements/import_entry.rs src/elements/export_entry.rs src/elements/global_entry.rs src/elements/ops.rs src/elements/func.rs src/elements/segment.rs src/elements/index_map.rs src/elements/name_section.rs src/elements/reloc_section.rs src/builder/mod.rs src/builder/invoke.rs src/builder/module.rs src/builder/code.rs src/builder/misc.rs src/builder/import.rs src/builder/memory.rs src/builder/table.rs src/builder/export.rs src/builder/global.rs src/builder/data.rs src/io.rs
+
+src/lib.rs:
+src/elements/mod.rs:
+src/elements/primitives.rs:
+src/elements/module.rs:
+src/elements/section.rs:
+src/elements/types.rs:
+src/elements/import_entry.rs:
+src/elements/export_entry.rs:
+src/elements/global_entry.rs:
+src/elements/ops.rs:
+src/elements/func.rs:
+src/elements/segment.rs:
+src/elements/index_map.rs:
+src/elements/name_section.rs:
+src/elements/reloc_section.rs:
+src/builder/mod.rs:
+src/builder/invoke.rs:
+src/builder/module.rs:
+src/builder/code.rs:
+src/builder/misc.rs:
+src/builder/import.rs:
+src/builder/memory.rs:
+src/builder/table.rs:
+src/builder/export.rs:
+src/builder/global.rs:
+src/builder/data.rs:
+src/io.rs:
