bad315ca44757b9f9b495ac549c6526ac8567a2e
mengya <zhang.9407@osu.edu>
2018-10-18 11:25:12 -0400

    data

--- target/debug/deps/data-e6e6bfa750d0106a.d
+++
@@ -1,5 +0,0 @@
-/home/mengya/parity-wasm/target/debug/deps/data-e6e6bfa750d0106a: src/bin/data.rs
-
-/home/mengya/parity-wasm/target/debug/deps/data-e6e6bfa750d0106a.d: src/bin/data.rs
-
-src/bin/data.rs:
